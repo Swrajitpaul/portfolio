4b8140d1f55e8074d4904d6161e58ee4182943ef
Swrajit Paul <swrajitpaul@gmail.com>
2023-05-07 15:04:54 -0400

    error

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https:/swrajitpaul.github.io/portfolio/'
+    ? 'https://swrajitpaul.github.io/portfolio/'
     : '/'
 })
+
